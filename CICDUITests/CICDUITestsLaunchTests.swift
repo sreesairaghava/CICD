174a5a3e570b92718f10ffd4136c4e7a143ce27a
Sree Sai Raghava <raghava.rym@gmail.com>
2022-02-14 18:16:32 +0530

    Initial Commit

---
+++ CICDUITests/CICDUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CICDUITestsLaunchTests.swift
+//  CICDUITests
+//
+//  Created by Sree Sai Raghava Dandu on 14/02/22.
+//
+
+import XCTest
+
+class CICDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
